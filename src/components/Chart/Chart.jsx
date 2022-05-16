36bdd9f315ed0faa80f505c8f371654369f2458a
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-05-16 05:28:46 -0400

    initial commit cards done

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+
+const Chart = () => {
+  const [dailyData, setDailyData] = useState()
+
+  
+
+  return (
+    <div>Chart</div>
+  )
+}
+
+export default Chart
